25f976a176e7ac52db172fa57d08e0b7e001a02f
Alchie Tagudin <alchie.tagudin@thepetlabco.com>
2023-04-23 22:41:42 +0800

    updates

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   webpack: (
     config,
     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
